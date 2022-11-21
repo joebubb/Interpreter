3bfe96474144fe721bd42e71fca9cd54fadb5d30
joebubb <jbubb@bu.edu>
2022-11-20 20:28:52 -0500

    part 1 done

--- interpreter.ml
+++ interpreter.ml
@@ -15,12 +15,19 @@
 (*NOTE: There are no restrictions on what you can use*)
 open Str 
 
+let str_regex = regexp "\"[a-zA-z]*\"[ ]*$"
+let int_regex = regexp "-?[1-9]+[0-9]*[ ]*$"
+
+let str_is_str s = string_match str_regex s 0 
+let str_is_int s = string_match int_regex s 0 
 
 (*Writing a line to a file*)
 let write_file_example (file_path: string) : unit =
   let fp = open_out file_path in
   let () = Printf.fprintf fp "writing this line!" in
     close_out fp
+
+let write_nothing = () 
 
 let (|>) a f = f a 
 
@@ -32,6 +39,28 @@ let rec map f ls =
 type const = 
   Int of string 
   | String of string 
+  | Undefined
+
+let extract_const c = 
+  match c with 
+  | Undefined -> ""
+  | Int(c) -> c 
+  | String(c) -> c 
+
+let rec str_of_stack stack = 
+  match stack with 
+  | [] -> ""
+  | h::t -> (extract_const h) ^ "\n" ^ (str_of_stack t)
+
+let write_stack stack output = 
+  let s = str_of_stack stack in 
+  let fp = open_out output in 
+  let _ = Printf.fprintf fp "%s" s in 
+  close_out fp
+let write_err output = 
+  let fp = open_out output in 
+  let _ = Printf.fprintf fp "\"Error\"" in 
+  close_out fp
 
 type com = 
   Push of const 
@@ -45,6 +74,9 @@ type com =
   | Concat 
   | Quit
   | Crash
+  | SoftCrash
+
+type status = Ok | Err 
 
 let push c = 
   match c with 
@@ -113,36 +145,179 @@ let str_to_com_constructor s =
   | "Quit" -> quit 
   | s -> crash 
 
+let const_of_str s = 
+  if str_is_str s then 
+    String(s)
+  else if str_is_int s then 
+    Int(s)
+  else
+    Undefined
+
 let next ls = 
   match ls with 
   | [] -> None 
-  | h::t -> Some(h)
+  | h::t -> Some(const_of_str h)
 
-let split_src_to_com_strings src = split (regexp "[\n]+") src 
+let split_by_newline s = split (regexp "[\n]+") s
 
-let trim_all ls = 
-  map (global_replace (regexp "[ ]+") "") ls
+let split_src_to_com_strings src = split_by_newline src 
    
+let split_by_spaces str = split (regexp "[ ]+") str
 
 let rec map_com_strings_to_coms str_ls = 
   match str_ls with 
-  | [] -> [Crash]
+  | [] -> [SoftCrash]
   | h::t -> 
     match h with 
     | [] -> Crash :: map_com_strings_to_coms t 
     | name::args -> ((str_to_com_constructor name) (next args))::(map_com_strings_to_coms t) 
+
+let concat_strings s1 s2 = 
+  let r = regexp "[\"]" in 
+  let f = global_replace r "" s1 in 
+  let s = global_replace r "" s2 in 
+  "\"" ^ f ^ s ^ "\""
+
+let rec stack_len stack = 
+  match stack with 
+  | [] -> 0 
+  | h::t -> 1 + (stack_len t)
+
+let top_two stack = 
+  let first = List.nth stack 0 in 
+  let second = List.nth stack 1 in 
+  (first, second)
+
+let stack_without_top_two stack = 
+  let t = List.tl stack in 
+  List.tl t 
+
+let both_ints f s = 
+  match f with 
+  | String(c) -> false 
+  | Undefined -> false
+  | Int(c) -> match s with 
+              | String(c) -> false
+              | Undefined -> false
+              | Int(c) -> true 
+
+let both_strs f s = 
+  match f with 
+  | Int(c) -> false 
+  | Undefined -> false
+  | String(c) -> match s with 
+              | Int(c) -> false
+              | Undefined -> false
+              | String(c) -> true 
+
+let push_to_stack c stack = (Ok, c :: stack)
+let pop_from_stack stack = 
+  match stack with 
+  | [] -> (Err, [])
+  | h::t -> (Ok, t)
+
+let add_with_stack stack = 
+  if stack_len stack < 2 then (Err, stack) else 
+  match top_two stack with 
+  | (f, s) -> if both_ints f s then 
+                let i1 = (int_of_string (extract_const f)) in 
+                let i2 = (int_of_string (extract_const s)) in 
+                let sum = i1 + i2 in 
+                (Ok, (Int(string_of_int sum))::(stack_without_top_two stack))
+              else
+                (Err, stack)
+
+let sub_with_stack stack = 
+  if stack_len stack < 2 then (Err, stack) else 
+    match top_two stack with 
+    | (f, s) -> if both_ints f s then 
+                  let i1 = (int_of_string (extract_const f)) in 
+                  let i2 = (int_of_string (extract_const s)) in 
+                  let diff = i1 - i2 in 
+                  (Ok, (Int(string_of_int diff))::(stack_without_top_two stack))
+                else
+                  (Err, stack)
+
+let mul_with_stack stack = 
+  if stack_len stack < 2 then (Err, stack) else 
+    match top_two stack with 
+    | (f, s) -> if both_ints f s then 
+                  let i1 = (int_of_string (extract_const f)) in 
+                  let i2 = (int_of_string (extract_const s)) in 
+                  let prod = i1 * i2 in 
+                  (Ok, (Int(string_of_int prod))::(stack_without_top_two stack))
+                else
+                  (Err, stack)
+
+let div_with_stack stack = 
+  if stack_len stack < 2 then (Err, stack) else 
+    match top_two stack with 
+    | (f, s) -> if (both_ints f s) && (not ((int_of_string (extract_const s))=0)) then 
+                  let i1 = (int_of_string (extract_const f)) in 
+                  let i2 = (int_of_string (extract_const s)) in 
+                  let quo = i1 / i2 in 
+                  (Ok, (Int(string_of_int quo))::(stack_without_top_two stack))
+                else
+                  (Err, stack)
+
+let swap_stack stack = 
+  if stack_len stack < 2 then (Err, stack) else 
+    match top_two stack with 
+    | (f, s) -> (Ok, s::f::(stack_without_top_two stack))   
+  
+let neg_stack stack = 
+  if stack_len stack < 1 then (Err, stack) else 
+    let top = List.nth stack 0 in 
+    if (str_is_int (extract_const top)) then 
+      let num = -1 * (int_of_string (extract_const top)) in 
+      let bottom = List.tl stack in 
+      let new_const = Int(string_of_int num) in
+      (Ok, new_const::bottom)
+    else 
+      (Err, stack)
+
+let concat_stack stack = 
+  if stack_len stack < 2 then (Err, stack) else 
+    match top_two stack with 
+    | (f, s) -> if both_strs f s then 
+      let s1 = extract_const f in 
+      let s2 = extract_const s in 
+      let ans = concat_strings s1 s2 in 
+      (Ok, String(ans)::(List.tl (List.tl stack)))
+    else
+      (Err, stack) 
+
+
+let rec interpret stack output comms = 
+  match comms with 
+  | [] -> write_stack stack output
+  | h::t -> 
+    match h with
+    | Push(c) -> let result = push_to_stack c stack in process_result result output t
+    | Pop -> let result = pop_from_stack stack in process_result result output t 
+    | Add -> let result = add_with_stack stack in process_result result output t 
+    | Sub -> let result = sub_with_stack stack in process_result result output t 
+    | Mul -> let result = mul_with_stack stack in process_result result output t 
+    | Div -> let result = div_with_stack stack in process_result result output t 
+    | Swap -> let result = swap_stack stack in process_result result output t
+    | Neg -> let result = neg_stack stack in process_result result output t  
+    | Concat -> let result = concat_stack stack in process_result result output t 
+    | Quit -> write_stack stack output 
+    | Crash -> write_err output 
+    | SoftCrash -> write_nothing 
+
+and process_result r output comms = 
+    match r with 
+    | (Err, s) -> write_err output
+    | (Ok, s) -> interpret s output comms
 
 (*When it comes to parsing src, you should find it useful that fold_left can be
   defined and used with strings (like String.fold_left in new OCaml version).
   See String.get. These are suggestions though and you are welcome
   to use what you want :)  *)
 let interpreter (src : string) (output_file_path: string): unit =
-  (* Split the string by lines *)
-  let stack = [] in 
-  let coms = 
-    split (regexp "[\n]+") src
-    |> trim_all 
-    |> map_com_strings_to_coms 
-    (* |> (interpret stack output_file_path) *)
-  in () 
-  
+  src
+  |> split_by_newline
+  |> (map split_by_spaces)
+  |> map_com_strings_to_coms 
+  |> (interpret [] output_file_path)
